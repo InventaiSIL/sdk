59f59d36a04b26c085e9eed5f02ef919ce205f17
SORGHO Charles <charles.sorgho@omnicell.fr>
2025-01-21 13:44:56 +0100

    feature: #1 - base interface for text ai agent and openai model encapsulation

---
+++ Inventai/Core/ITextAgentBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Inventai.Core;
+
+/// <summary>
+/// Base interface to be implemented by the text agents
+/// </summary>
+public interface ITextAgent
+{
+    /// <summary>
+    /// Completes <paramref name="pMessage"/> as per the LLM model
+    /// </summary>
+    /// <param name="pMessage"></param>
+    /// <returns></returns>
+    public string CompleteMessage(string pMessage);
+}
---
+++ InventaiTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Inventai.TextAgents;
+using Inventai.Core;
+
+try
+{
+    TextAgentOpenAI agent = new(EOpenAITextModels.GPT4omini);
+    Console.WriteLine(agent.CompleteMessage("Say 'This is a test.'"));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
